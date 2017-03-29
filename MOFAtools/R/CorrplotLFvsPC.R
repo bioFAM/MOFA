ac3be058c356558c317ecd560bf6fff9ac26478f
bv2 <britta.velten@embl.de>
2017-03-29 19:40:17 +0200

    Added new files

---
+++ MOFAtools/R/CorrplotLFvsPC.R
@@ -0,0 +1,28 @@
+library(corrplot)
+
+#' @title Correlate latent factors to principal components of specified view
+#' @name CorrplotLFvsPC
+#' @description fill this
+#' @param object a \code{\link{MOFAmodel}} object.
+#' @param viewname4PC name of view to do PCA on
+#' @param noPCs number of PCs to compare to (default = 5)
+#' @details asd
+#' @return Correlation Matrix Latent factors versus Principal components
+#' @reference fill this
+#' @import corrplot
+#' @export
+
+CorrplotLFvsPC<-function(modelobject, viewname4PC, noPCs=5){
+  Z<-modelobject@Expectations$Z$E
+  singleview<-modelobject@TrainData[[viewname4PC]]
+  pc.out<-prcomp(singleview)
+  
+  corrmatrix<-apply(pc.out$x[,1:noPCs],2, function(pc) {
+    apply(Z,2, function(lv){
+      cor(pc, lv)
+    })
+  })
+  
+  corrplot::corrplot(corrmatrix, order="original", title=viewname4PC,mar = c(1, 1, 3, 1))
+  return(corrmatrix)
+}

b3ff2f2d7148497051f38de21fad6ee9c236736b
rargelaguet <ricard.argelaguet@gmail.com>
2017-03-29 10:04:45 +0100

    Updating Non-sparse model

--- scGFA/test/nonsparse_test.py
+++ scGFA/test/nonsparse_test.py
@@ -15,22 +15,24 @@ from scGFA.core.simulate import Simulate
 from scGFA.core.BayesNet import BayesNet
 from scGFA.core.utils import *
 from scGFA.core.multiview_nodes import *
-from scGFA.core.seeger_nodes import Binomial_PseudoY_Node, Poisson_PseudoY_Node, Bernoulli_PseudoY_Node
+from scGFA.core.seeger_nodes import *
 from scGFA.core.nodes import Constant_Node
-from scGFA.core.nonsparse_updates import Y_Node, Alpha_Node, W_Node, Tau_Node, Z_Node, Y_Node
+from scGFA.core.nonsparse_updates import *
+
+from scGFA.run.init_nodes import *
 
 ###################
 ## Generate data ##
 ###################
 
 # Define dimensionalities
-M = 1
+M = 3
 N = 100
-D = s.asarray([100,])
+D = s.asarray([1000,1000,1000])
 K = 6
 
 
-## Simulate data  ##
+# Simulate data
 data = {}
 tmp = Simulate(M=M, N=N, D=D, K=K)
 
@@ -44,39 +46,32 @@ data['Z'][:,5] = stats.norm.rvs(loc=0, scale=1, size=N)
 
 data['alpha'] = s.zeros((M,K))
 data['alpha'][0,:] = [1,1,1e6,1,1e6,1e6]
-# data['alpha'][1,:] = [1,1e6,1,1e6,1,1e6]
-# data['alpha'][2,:] = [1e6,1,1,1e6,1e6,1]
+data['alpha'][1,:] = [1,1e6,1,1e6,1,1e6]
+data['alpha'][2,:] = [1e6,1,1,1e6,1e6,1]
 
 data['W'], _ = tmp.initW_ard(alpha=data['alpha'])
 data['mu'] = [ s.zeros(D[m]) for m in xrange(M)]
 # data['tau']= [ s.ones(D[m])*1000 for m in xrange(M) ]
 data['tau']= [ stats.uniform.rvs(loc=1,scale=3,size=D[m]) for m in xrange(M) ]
+
+missingness = 0.0
 Y_gaussian = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
-	likelihood="gaussian", missingness=0.00)
+	likelihood="gaussian", missingness=missingness)
 Y_poisson = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
-	likelihood="poisson", missingness=0.00)
+	likelihood="poisson", missingness=missingness)
 Y_bernoulli = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
-	likelihood="bernoulli", missingness=0.0)
+	likelihood="bernoulli", missingness=missingness)
 # Y_binomial = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
 	# likelihood="binomial", missingness=0.0, min_trials=10, max_trials=50)
 
-# data["Y"] = ( Y_gaussian[0], Y_poisson[1], Y_bernoulli[2] )
-# data["Y"] = Y_bernoulli
-# data["Y"] = Y_poisson
-# data["Y"] = Y_binomial
-data["Y"] = Y_gaussian
+data = Y_gaussian
 
-# likelihood = ["gaussian","gaussian","gaussian"]
-likelihood = ["gaussian"]
-view_names = ["gaussian"]
-# view_names = ["gaussian1","gaussian2","gaussian3"]
-
-#################################
-## Initialise Bayesian Network ##
-#################################
+######################
+## Initialise model ##
+######################
 
 # Define initial number of latent variables
-K = 10
+K = 8
 
 # Define model dimensionalities
 dim = {}
@@ -85,121 +80,98 @@ dim["N"] = N
 dim["D"] = D
 dim["K"] = K
 
-###############
-## Add nodes ##
-###############
 
-# Z without covariates (variational node)
-Z_qmean = s.stats.norm.rvs(loc=0, scale=1, size=(N,K))
-Z_qcov = s.repeat(s.eye(K)[None,:,:],N,0)
-Z = Z_Node(dim=(N,K), pmean=s.nan, pcov=s.nan, qmean=Z_qmean, qcov=Z_qcov, qE=Z_qmean)
+##############################
+## Define the model options ##
+##############################
 
-Z.updateExpectations()
-
-# Z with covariates (variational node)
-# Z_qmean = s.stats.norm.rvs(loc=0, scale=1, size=(N,K-1))
-# Z_qmean = s.c_[ Z_qmean, s.asarray([True,False]*(N/2), dtype=s.float32) ]
-# Z_qcov = s.repeat(s.eye(K)[None,:,:],N,0)
-# Z = Z_Node(dim=(N,K), qmean=Z_qmean, qcov=Z_qcov)
-# Z.updateExpectations()
-# Z.setCovariates(idx=K-1)
+model_opts = {}
+model_opts['likelihood'] = ['gaussian']* M 
+model_opts['k'] = K
 
 
-# alpha (variational node)
-alpha_list = [None]*M
-alpha_pa = 1e-14
-alpha_pb = 1e-14
-# alpha_qa = s.ones(K)
-alpha_qb = s.ones(K)
-alpha_qE = s.ones(K)*100
-for m in xrange(M):
-	alpha_qa = alpha_pa + s.ones(K)*D[m]/2
-	alpha_list[m] = Alpha_Node(dim=(K,), pa=alpha_pa, pb=alpha_pb, qa=alpha_qa[m], qb=alpha_qb, qE=alpha_qE)
-alpha = Multiview_Variational_Node((K,)*M, *alpha_list)
+# Define priors
+model_opts["priorZ"] = { 'mean':0., 'cov':s.eye(K) }
+model_opts["priorAlpha"] = { 'a':[1e-14]*M, 'b':[1e-14]*M }
+model_opts["priorW"] = { 'mean':[s.nan]*M, 'cov':[s.nan]*M }
+model_opts["priorTau"] = { 'a':[1e-14]*M, 'b':[1e-14]*M }
 
+# Define initialisation options
+model_opts["initZ"] = { 'mean':"random", 'cov':s.eye(K), 'E':None, 'E2':None }
+model_opts["initAlpha"] = { 'a':[s.nan]*M, 'b':[s.nan]*M, 'E':[100.]*M }
+model_opts["initW"] = { 'mean':["random"]*M, 'cov':[s.eye(K)*x for x in model_opts["initAlpha"]['E']], 'E':[None]*M}
+model_opts["initTau"] = { 'a':[s.nan]*M, 'b':[s.nan]*M, 'E':[100.]*M }
 
-# W (variational node)
-W_list = [None]*M
-for m in xrange(M):
-	W_qmean = s.stats.norm.rvs(loc=0, scale=1, size=(D[m],K))
-	W_qcov = s.repeat(a=s.eye(K)[None,:,:], repeats=D[m] ,axis=0)
-	W_list[m] = W_Node(dim=(D[m],K), pmean=s.nan, pcov=s.nan, qmean=W_qmean, qcov=W_qcov, qE=W_qmean)
-W = Multiview_Variational_Node(M, *W_list)
+# Define covariates
+model_opts['covariates'] = None
 
+# Define schedule of updates
+model_opts['schedule'] = ("W","Z","Alpha","Tau","Y")
 
-# tau/kappa (mixed node)
-tau_list = [None]*M
-for m in xrange(M):
-	if likelihood[m] == "poisson":
-		tmp = 0.25 + 0.17*s.amax(data["Y"][m],axis=0)
-		tau_list[m] = Observed_Local_Node(dim=(D[m],), value=tmp)
-	elif likelihood[m] == "bernoulli":
-		tmp = s.ones(D[m])*0.25
-		tau_list[m] = Observed_Local_Node(dim=(D[m],), value=tmp)
-	elif likelihood[m] == "binomial":
-		tmp = 0.25*s.amax(data["Y"]["tot"][m],axis=0)
-		tau_list[m] = Observed_Local_Node(dim=(D[m],), value=tmp)
-	elif likelihood[m] == "gaussian":
-		tau_pa = 1e-14
-		tau_pb = 1e-14
-		tau_qa = tau_pa + s.ones(D[m])*N/2
-		tau_qb = s.zeros(D[m])
-		tau_qE = s.zeros(D[m]) + 100
-		tau_list[m] = Tau_Node(dim=(D[m],), pa=tau_pa, pb=tau_pb, qa=tau_qa, qb=tau_qb, qE=tau_qE)
-tau = Multiview_Mixed_Node(M,*tau_list)
-
-
-
-# Y/Yhat (mixed node)
-Y_list = [None]*M
-for m in xrange(M):
-	if likelihood[m] == "gaussian":
-		Y_list[m] = Y_Node(dim=(N,D[m]), obs=data['Y'][m])
-	elif likelihood[m] == "poisson":
-		Y_list[m] = Poisson_PseudoY_Node(dim=(N,D[m]), obs=data['Y'][m], Zeta=None, E=None)
-	elif likelihood[m] == "bernoulli":
-		Y_list[m] = Bernoulli_PseudoY_Node(dim=(N,D[m]), obs=data['Y'][m], Zeta=None, E=None)
-	elif likelihood[m] == "binomial":
-		Y_list[m] = Binomial_PseudoY_Node(dim=(N,D[m]), tot=data['Y']["tot"][m], obs=data['Y']["obs"][m], Zeta=None, E=None)
-Y = Multiview_Mixed_Node(M, *Y_list)
-
-
-############################
-## Define Markov Blankets ##
-############################
-
-Z.addMarkovBlanket(W=W, tau=tau, Y=Y)
-for m in xrange(M):
-	alpha.nodes[m].addMarkovBlanket(W=W.nodes[m])
-	W.nodes[m].addMarkovBlanket(Z=Z, tau=tau.nodes[m], alpha=alpha.nodes[m], Y=Y.nodes[m])
-	if likelihood[m] == "gaussian":
-		Y.nodes[m].addMarkovBlanket(Z=Z, W=W.nodes[m], tau=tau.nodes[m])
-		tau.nodes[m].addMarkovBlanket(W=W.nodes[m], Z=Z, Y=Y.nodes[m])
-	else:
-		Y.nodes[m].addMarkovBlanket(Z=Z, W=W.nodes[m], kappa=tau.nodes[m])
-
-
-# Define update schedule
-schedule = ["Y","W","Z","alpha","tau"]
-nodes = { "W":W, "tau":tau, "Z":Z, "Y":Y, "alpha":alpha }
 
 #############################
 ## Define training options ##
 #############################
 
-options = {}
-options['maxiter'] = 500
-options['tolerance'] = 1E-2
-options['forceiter'] = False
-options['elbofreq'] = 1
-options['dropK'] = { 'by_norm':None, 'by_pvar':None, 'by_cor':None }
-options['savefreq'] = options['maxiter']+1
-options['savefolder'] = "/tmp/test"
-options['verbosity'] = 2
+train_opts = {}
+train_opts['elbofreq'] = 1
+train_opts['maxiter'] = 200
+train_opts['tolerance'] = 1E-2
+train_opts['forceiter'] = True
+# train_opts['dropK'] = { "by_norm":0.05, "by_pvar":None, "by_cor":None } # IT IS NOT WORKING
+train_opts['dropK'] = { "by_norm":None, "by_pvar":None, "by_cor":None }
+train_opts['savefreq'] = s.nan
+train_opts['savefolder'] = s.nan
+train_opts['verbosity'] = 2
+# train_opts['trials'] = 1
+# cores = 1
+# keep_best_run = False
+
+
 
 ####################
 ## Start training ##
 ####################
 
-net = BayesNet(dim=dim, schedule=schedule, nodes=nodes, options=options)
+# runMultipleTrials(data_opts, model_opts, train_opts, cores, keep_best_run)
+# runSingleTrial(data["Y"], model_opts, train_opts)
+
+init = init_nonsparse(dim, data, model_opts["likelihood"])
+
+
+init.initZ(pmean=model_opts["priorZ"]["mean"], pcov=model_opts["priorZ"]["cov"],
+           qmean=model_opts["initZ"]["mean"], qcov=model_opts["initZ"]["cov"], qE=model_opts["initZ"]["E"],
+           covariates=model_opts['covariates'])
+
+init.initW(pmean=model_opts["priorW"]["mean"], pcov=model_opts["priorW"]["cov"],
+           qmean=model_opts["initW"]["mean"], qcov=model_opts["initW"]["cov"], qE=model_opts["initW"]["E"])
+
+init.initAlpha(pa=model_opts["priorAlpha"]['a'], pb=model_opts["priorAlpha"]['b'],
+               qa=model_opts["initAlpha"]['a'], qb=model_opts["initAlpha"]['b'], qE=model_opts["initAlpha"]['E'])
+
+
+init.initTau(pa=model_opts["priorTau"]['a'], pb=model_opts["priorTau"]['b'],
+             qa=model_opts["initTau"]['a'], qb=model_opts["initTau"]['b'], qE=model_opts["initTau"]['E'])
+
+init.initY()
+
+# Define the markov blanket of each node
+# print "Defining Markov Blankets...\n"
+init.MarkovBlanket()
+
+# Initialise Bayesian Network
+# print "Initialising Bayesian network...\n"
+net = BayesNet(dim=dim, trial=1, schedule=model_opts["schedule"], nodes=init.getNodes(), options=train_opts)
+
+# Start training
 net.iterate()
+
+# print net.getNodes()["Z"].getParameters()["cov"]
+# print net.getNodes()["Z"].getParameters()["cov"][0]
+Z = net.getNodes()["Z"].getExpectations()["E"]
+print s.absolute(corr(Z.T,Z.T))
+# print s.diag(net.getNodes()["Z"].getParameters()["cov"][0])
+
+# print "Number of factors:"
+# print s.absolute(net.getNodes()["Z"].getExpectations()["E"]).mean(axis=0)
+# print s.sum( s.absolute(net.getNodes()["Z"].getExpectations()["E"]).mean(axis=0) > 0.01)

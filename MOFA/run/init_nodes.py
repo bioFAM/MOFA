scaling covariates, fixed small bug whtn shutting down factors and updated R package

MOFA/run/init_nodes.py
@@ -35,7 +35,7 @@ class initModel(object):
         # Set the seed
         s.random.seed(seed)
 
-    def initZ(self, pmean, pvar, qmean, qvar, qE=None, qE2=None, covariates=None):
+    def initZ(self, pmean, pvar, qmean, qvar, qE=None, qE2=None, covariates=None, scale_covariates=None):
         # Method to initialise the latent variables
         # Inputs:
         # - covariates (nd array): matrix of covariates with dimensions (nsamples,ncovariates)
@@ -68,7 +68,12 @@ class initModel(object):
 
         # Add covariates
         if covariates is not None:
+            assert scale_covariates != None, "If you use covariates also define data_opts['scale_covariates']"
             idx_covariates = s.array(range(covariates.shape[1]))
+            # Center and scale the covariates to match the prior distribution N(0,1)
+            # to-do: this needs to be improved to take the particular mean and var into account
+            # covariates[scale_covariates] = (covariates - covariates.mean(axis=0)) / covariates.std(axis=0)
+            covariates[:,scale_covariates] = (covariates[:,scale_covariates] - covariates[:,scale_covariates].mean(axis=0)) / covariates[:,scale_covariates].std(axis=0)
             qmean[:,idx_covariates] = covariates
         else:
             idx_covariates = None
@@ -77,7 +82,6 @@ class initModel(object):
         # self.Z = Constant_Node(dim=(self.N,self.K), value=qmean)
         self.Z = Z_Node(dim=(self.N,self.K),
                         pmean=s.ones((self.N,self.K))*pmean,
-                        # pvar=s.ones((self.N,self.K))*pvar,
                         pvar=s.ones((self.K,))*pvar,
                         qmean=s.ones((self.N,self.K))*qmean,
                         qvar=s.ones((self.N,self.K))*qvar,

MOFA/test/test.py
@@ -34,7 +34,7 @@ N = 100
 # D = s.asarray([10000,10000,10000])
 # D = s.asarray([500,])
 D = s.asarray([500,500,500])
-K = 6
+K = 7
 
 
 ## Simulate data  ##
@@ -52,19 +52,19 @@ data['Z'] = stats.norm.rvs(loc=0, scale=1, size=(N,K))
 
 
 data['alpha'] = [ s.zeros(K,) for m in xrange(M) ]
-data['alpha'][0] = [1,1,1e6,1,1e6,1e6]
-data['alpha'][1] = [1,1e6,1,1e6,1,1e6]
-data['alpha'][2] = [1e6,1,1,1e6,1e6,1]
+data['alpha'][0] = [1,1,1,1e6,1,1e6,1e6]
+data['alpha'][1] = [1,1,1e6,1,1e6,1,1e6]
+data['alpha'][2] = [1,1e6,1,1,1e6,1e6,1]
 
-theta = [ s.ones((D[m],K))*0.5 for m in xrange(M) ]
+theta = [ s.ones((D[m],K))*1.0 for m in xrange(M) ]
 data['S'], data['W'], data['W_hat'], _ = tmp.initW_spikeslab(theta=theta, alpha=data['alpha'], annotation=True)
 
-data['mu'] = [ s.ones(D[m])*3. for m in xrange(M)]
+data['mu'] = [ s.ones(D[m])*0. for m in xrange(M)]
 data['tau']= [ stats.uniform.rvs(loc=1,scale=3,size=D[m]) for m in xrange(M) ]
 # data['tau']= [ stats.uniform.rvs(loc=0.1,scale=3,size=D[m]) for m in xrange(M) ]
 
-missingness = 0.5
-missing_view = 0.1
+missingness = 0.0
+missing_view = 0.0
 # Y_warp = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
 #   likelihood="warp", missingness=missingness, missing_view=missing_view)
 Y_gaussian = tmp.generateData(W=data['W'], Z=data['Z'], Tau=data['tau'], Mu=data['mu'],
@@ -93,10 +93,11 @@ data["Y"] = ( Y_gaussian[0], Y_gaussian[1], Y_gaussian[2] )
 
 data_opts = {}
 data_opts["outfile"] = "/tmp/test.h5"
-data_opts['view_names'] = ["g","p","b"]
-data_opts['center'] = [False]*M
+data_opts['view_names'] = ["View 1","View 2","View 3"]
+data_opts['center'] = [True]*M
 data_opts['scale_views'] = [False]*M
-data_opts['covariates'] = None
+data_opts['covariates'] = stats.bernoulli.rvs(0.5, size=N)[:,None]
+data_opts['scale_covariates'] = [True]
 
 # Center the data
 if data_opts['center'][m]:
@@ -142,11 +143,14 @@ if data_opts['covariates'] is not None:
 # Define whether to learn means
 model_opts["learnMean"] = True
 if model_opts["learnMean"]:
-  # MODIFY THIS
-  data_opts['covariates'] = s.ones((dim["N"],1))
+  if data_opts['covariates'] is None:
+    data_opts['covariates'] = s.ones((dim["N"],1))
+    data_opts['scale_covariates'] = False
+  else:
+    data_opts['covariates'] = s.concatenate((data_opts['covariates'],s.ones((dim["N"],1))), axis=1)
+    data_opts['scale_covariates'].append(False)
   dim["K"] += data_opts['covariates'].shape[1]
   K = dim["K"]
-  # model_opts['nonsparse_covariates'] = [True]
 
 
 
@@ -164,8 +168,7 @@ model_opts['ardZ'] = False
 model_opts['ardW'] = "mk"
 
 # Define for which factors to learn Theta
-# model_opts['learnTheta'] = s.ones((M,K))
-model_opts['learnTheta'] = s.zeros((M,K))
+model_opts['learnTheta'] = [s.zeros(K) for m in xrange(M)]
 
 # Define schedule of updates
 # model_opts['schedule'] = ["Y","SW","Z","AlphaW","Theta","Tau"]
@@ -195,10 +198,10 @@ elif model_opts['ardW'] == "mk":
 
 
 # Theta
-model_opts["priorTheta"] = { 'a':[s.ones(K) for m in xrange(M)], 'b':[s.ones(K) for m in xrange(M)] }
+model_opts["priorTheta"] = { 'a':[s.ones(K,) for m in xrange(M)], 'b':[s.ones(K,) for m in xrange(M)] }
 for m in xrange(M):
   for k in xrange(K):
-    if model_opts['learnTheta'][m,k]==0:
+    if model_opts['learnTheta'][m][k]==0:
       model_opts["priorTheta"]["a"][m][k] = s.nan
       model_opts["priorTheta"]["b"][m][k] = s.nan
 
@@ -213,7 +216,7 @@ model_opts["priorTau"] = { 'a':[s.ones(D[m])*1e-3 for m in xrange(M)], 'b':[s.on
 
 # Latent variables
 # model_opts["initZ"] = { 'mean':"orthogonal", 'var':s.ones((N,K)), 'E':None, 'E2':None }
-model_opts["initZ"] = { 'mean':"random", 'var':s.ones((N,K)), 'E':None, 'E2':None }
+model_opts["initZ"] = { 'mean':"random", 'var':s.ones((K,)), 'E':None, 'E2':None }
 if model_opts['ardZ']:
   model_opts["initAlphaZ"] = { 'a':s.nan, 'b':s.nan, 'E':s.ones(K)*100 }
 
@@ -222,27 +225,29 @@ if model_opts['ardW'] == "m":
   # model_opts["initAlphaW"] = { 'a':[s.nan]*M, 'b':[s.nan]*M, 'E':[1.]*M }
   model_opts["initAlphaW"] = { 'a':[s.nan]*M, 'b':[s.nan]*M, 'E':[ K*D[m]/(data[m].std(axis=0)**2 - 1./model_opts["initTau"]["E"][m]).sum() for m in xrange(M) ] }
 elif model_opts['ardW'] == "k":
-  model_opts["initAlphaW"] = { 'a':s.nan*s.ones(K), 'b':s.nan*s.ones(K), 'E':s.ones(K) }
+  model_opts["initAlphaW"] = { 'a':s.nan*s.ones(K), 'b':s.nan*s.ones(K), 'E':s.ones(K)*100 }
 elif model_opts['ardW'] == "mk":
   model_opts["initAlphaW"] = { 'a':[s.nan]*M, 'b':[s.nan]*M, 'E':[1.*s.ones(K) for m in xrange(M)] }
 
-model_opts["initSW"] = { 'Theta':[0.5*s.ones((D[m],K)) for m in xrange(M)],
-                          'mean_S0':[s.zeros((D[m],K)) for m in xrange(M)],
-                          'var_S0':[s.nan*s.ones((D[m],K)) for m in xrange(M)],
-                          'mean_S1':[s.zeros((D[m],K)) for m in xrange(M)], # (TO-DO) allow also random
-                          # 'mean_S1':[s.ones((D[m],K)) for m in xrange(M)],
-                          'var_S1':[s.ones((D[m],K)) for m in xrange(M)],
-                          'ES':[None]*M, 'EW_S0':[None]*M, 'EW_S1':[None]*M }
-
-
 # Theta
-model_opts["initTheta"] = { 'a':[s.ones(K) for m in xrange(M)], 'b':[s.ones(K) for m in xrange(M)], 'E':[s.zeros(K)*s.nan for m in xrange(M)] }
+model_opts["initTheta"] = { 'a':[s.ones(K,) for m in xrange(M)], 'b':[s.ones(K,) for m in xrange(M)], 'E':[1.*s.ones((D[m],K)) for m in xrange(M)] }
 for m in xrange(M):
   for k in xrange(K):
-    if model_opts['learnTheta'][m,k]==0.:
+    if model_opts['learnTheta'][m][k]==0.:
       model_opts["initTheta"]["a"][m][k] = s.nan
       model_opts["initTheta"]["b"][m][k] = s.nan
-      model_opts["initTheta"]["E"][m][k] = 1.0
+      # model_opts["initTheta"]["E"][m][:,k] = 1.0
+
+
+# Weights
+model_opts["initSW"] = { 
+  'Theta':[0.5*s.ones((D[m],K)) for m in xrange(M)],
+  'mean_S0':[s.zeros((D[m],K)) for m in xrange(M)],
+  'var_S0':[s.nan*s.ones((D[m],K)) for m in xrange(M)],
+  'mean_S1':[s.zeros((D[m],K)) for m in xrange(M)], # (TO-DO) allow also random
+  'var_S1':[s.ones((D[m],K)) for m in xrange(M)],
+  'ES':[None]*M, 'EW_S0':[None]*M, 'EW_S1':[None]*M }
+
 
 
 # Noise
@@ -268,7 +273,7 @@ if data_opts['covariates'] is not None:
   ## Variational distributions (Q) ##
   # Latent variables
   # model_opts["initZ"]["mean"][:,idx] = model_opts["covariates"]
-  model_opts["initZ"]["var"][:,idx] = 0.
+  model_opts["initZ"]["var"][idx] = 0.
   if model_opts['ardZ']:
         model_opts["initAlphaZ"]["E"][idx] = s.nan
 
@@ -298,20 +303,21 @@ if data_opts['covariates'] is not None:
 
 
 if model_opts["learnMean"]:
-  model_opts['learnTheta'][:,0] = 0.
-
   for m in range(M):
+
     # Weights
     if model_opts['likelihood'][m]=="gaussian":
       model_opts["initSW"]["mean_S1"][m][:,0] = data["Y"][m].mean(axis=0)
       model_opts["initSW"]["var_S1"][m][:,0] = 1e-5
+
     # Theta
+    model_opts['learnTheta'][m][0] = 0.
     model_opts["initSW"]["Theta"][m][:,0] = 1.
     model_opts["priorTheta"]['a'][m][0] = s.nan
     model_opts["priorTheta"]['b'][m][0] = s.nan
     model_opts["initTheta"]["a"][m][0] = s.nan
     model_opts["initTheta"]["b"][m][0] = s.nan
-    model_opts["initTheta"]["E"][m][0] = 1.
+    model_opts["initTheta"]["E"][m][:,0] = 1.
 
 
 #############################
@@ -324,7 +330,7 @@ train_opts['maxiter'] = 500
 # train_opts['tolerance'] = 1E-2
 train_opts['tolerance'] = 0.01
 train_opts['forceiter'] = True
-train_opts['drop'] = { "by_norm":None, "by_pvar":None, "by_cor":None, "by_r2":None }
+train_opts['drop'] = { "by_norm":None, "by_pvar":None, "by_cor":None, "by_r2":0.01 }
 train_opts['startdrop'] = 10
 train_opts['freqdrop'] = 1
 train_opts['savefreq'] = s.nan
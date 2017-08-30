219ccf1d201a7dd7a232c5e7bfdd06655159f458
rargelaguet <ricard.argelaguet@gmail.com>
2017-08-30 12:46:53 +0100

    asd

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup
 from setuptools import find_packages
 
+def setup_package():
+  install_requires = ['pandas', 'scipy', 'numpy', 'sklearn', 'argparse', 'h5py']
+  console_scripts = [ 'mofa=mofa.run.template_run:entry_point'],
+
+  metadata = dict(
+      name = 'MOFA',
+      version = '0.1',
+      description = 'Multi-Omics Factor Analysis',
+      #long_description=read('README.rst'),
+      url = 'http://github.com/rargelaguet/mofa',
+      author = 'Ricard Argelaguet, Damien Arnol and Britta Velten',
+      author_email = 'ricard.argelaguet@gmail.com',
+      license = 'MIT',
+      packages = find_packages(),
+      install_requires = install_requires,
+      entry_points = {'console_scripts': console_scripts}
+    )
+
+  setup(**metadata)
+
 if __name__ == '__main__':
-    setup(name='MOFA',
-          version='0.1',
-          description='Multi-Omics Factor Analysis',
-          #long_description=read('README.rst'),
-          url='http://github.com/rargelaguet/mofa',
-          author='Ricard Argelaguet, Damien Arnol and Britta Velten',
-          author_email='ricard.argelaguet@gmail.com',
-          license='MIT',
-          packages=find_packages(),
-		      install_requires=['pandas', 'scipy', 'numpy', 'sklearn', 'argparse', 'h5py'],
-          console_scripts = [ 'mofa=mofa.run.template_run:entry_point'],
-          entry_points={'console_scripts': console_scripts}
-        )
+  setup_package()

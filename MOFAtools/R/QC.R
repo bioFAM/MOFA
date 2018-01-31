4256edf968d1850095b786f33b520da5f7813dd9
rargelaguet <ricard.argelaguet@gmail.com>
2018-01-31 18:09:49 +0000

    small bug fixed

--- MOFAtools/R/QC.R
+++ MOFAtools/R/QC.R
@@ -45,7 +45,7 @@ qualityControl <- function(object, verbose = F) {
   # Check that there are no features with complete missing values
   if (verbose==T) message("Checking there are no features with complete missing values...")
   for (view in viewNames(object)) {
-    if (any(apply(object@TrainData[[view]],1, function(x) mean(is.na(x))) == 1)) {
+    if (!all(apply(object@TrainData[[view]],1, function(x) mean(is.na(x))) < 1)) {
       print("Warning: you have features which only contain missing values, consider removing them...")
     }
   }
@@ -53,7 +53,7 @@ qualityControl <- function(object, verbose = F) {
   # Check that there are no features with zero variance
   if (verbose==T) message("Checking there are no features with zero variance...")
   for (view in viewNames(object)) {
-    if (any(apply(object@TrainData[[view]],1,var,na.rm=T) == 0)) {
+    if (!all(apply(object@TrainData[[view]],1,var,na.rm=T) > 0)) {
       print("Warning: you have features with zero variance, consider removing them...")
     }
   }

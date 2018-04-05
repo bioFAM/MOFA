c78f7de54160d532f28e8d62a73086fe2458676c
Britta Velten <britta.velten@embl.de>
2018-04-05 10:48:50 +0200

    fixed bug in subsetting

--- MOFAtools/R/subset.R
+++ MOFAtools/R/subset.R
@@ -8,16 +8,23 @@
 #' @description Method to subset (or sort) factors
 #' @param object a \code{\link{MOFAmodel}} object.
 #' @param factors character vector with the factor names, or numeric vector with the index of the factors.
+#' @param keep_intercept bool whether intercept is kept when subsetting (default TRUE).
 #' @export
 
-subsetFactors <- function(object, factors) {
+subsetFactors <- function(object, factors, keep_intercept=T) {
   
   # Sanity checks
   if (class(object) != "MOFAmodel") stop("'object' has to be an instance of MOFAmodel")
   stopifnot(length(factors) <= object@Dimensions[["K"]])
-  if (is.character(factors)) stopifnot(all(factors %in% factorNames(object)))
-  if (is.numeric(factors)) stopifnot(all(factors %in% 1:object@Dimensions[["K"]]))
-  
+
+    # Get factors
+   if(is.numeric(factors)) {
+      if (object@ModelOpts$learnIntercept == T) factors <- factorNames(object)[factors+1]
+      else factors <- factorNames(object)[factors]
+    }
+      else{ stopifnot(all(factors %in% factorNames(object))) }
+
+  if(keep_intercept & object@ModelOpts$learnIntercept == T & !"intercept" %in% factors) factors <- c("intercept", factors)
   # Subset expectations
   object@Expectations$Z <- object@Expectations$Z[,factors, drop=F]
   object@Expectations$AlphaW <- sapply(object@Expectations$AlphaW, function(x) x[factors], simplify = F, USE.NAMES = T)
@@ -28,7 +35,7 @@ subsetFactors <- function(object, factors) {
   object@Dimensions[["K"]] <- length(factors)
   
   # Modify factor names
-  factorNames(object) <- as.character(1:object@Dimensions[["K"]])
+  factorNames(object) <- as.character(factors)
   
   return(object)
 }
